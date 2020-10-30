3d19ec384dda22eec5de4bc188367bb91f221c86
govvc <64343137+govvc@users.noreply.github.com>
2020-10-30 22:19:06 +0800

    Add files via upload

---
+++ LogisticRevised.py
@@ -0,0 +1,129 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import animation
+
+X_train = np.loadtxt("F:/ML_DATA/Exam/train/x.txt")
+y_train = np.loadtxt("F:/ML_DATA/Exam/train/y.txt")
+
+X_test = np.loadtxt("F:/ML_DATA/Exam//test/x.txt")
+y_test = np.loadtxt("F:/ML_DATA/Exam/test/y.txt")
+
+
+def standardize(z):
+    return (z - np.mean(z)) / np.std(z)
+
+
+class logistic:
+    def __init__(self, lr, epochs, X, y):
+        self.lr = lr
+        self.epochs = epochs
+        self.X = X
+        self.y = y
+
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(z))
+
+    def fit(self):
+        """
+        train the dataset
+        """
+
+        x = np.insert(self.X, 0, 1, axis=1)
+        np.random.seed(4)
+        self.loss = []
+        self.theta = []
+        w = np.random.rand(x.shape[1])
+
+        for i in range(0, self.epochs):
+            h = self.sigmoid(np.dot(x, w.T))
+            J = np.sum(np.dot(self.y, np.log(h)) + np.dot(1 - self.y, np.log(1 - h)))
+            self.loss.append(-J)
+            print('---------------------\n', -J)
+            w = w - self.lr * np.dot(self.y - h, x)
+
+            self.theta.append(w)
+
+    def predict(self, x, y):
+        """
+
+        :return: prediction
+        """
+
+        predict_, ac_ = [], []
+        for i in np.arange(self.epochs):
+            h = self.sigmoid(np.dot(x, self.theta[i]))
+
+            for i in range(len(h)):
+                if h[i] > (1 - h[i]):
+                    predict_.append(1)
+                else:
+                    predict_.append(0)
+
+            ac_.append(np.sum(predict_ == y) / len(predict_))
+            predict_.clear()
+
+        return ac_
+
+
+if __name__ == '__main__':
+    X_train_1 = standardize(X_train)
+    log = logistic(0.001, 500, X_train_1, y_train)
+    log.fit()
+
+    # Preparation for drawing
+    X_train_1 = np.insert(X_train_1, 0, 1, axis=1)
+    ac_train = log.predict(X_train_1, y_train)
+    X_test_1 = standardize(X_test)
+    X_test_1 = np.insert(X_test_1, 0, 1, axis=1)
+    ac_test = log.predict(X_test_1, y_test)
+
+    fig, axes = plt.subplots(1, 3)
+
+    axes[0].set_xlim([0, 500])
+    axes[0].set_ylim([0, 80])
+    axes[0].set_xlabel('Iteration')
+    axes[0].set_ylabel('Loss')
+    axes[0].set_title('Loss')
+
+    axes[1].set_xlim([0, 500])
+    axes[1].set_ylim([0.2, 1])
+    axes[1].set_xlabel('Iteration')
+    axes[1].set_ylabel('Accuracy')
+    axes[1].set_title('Accuracy')
+
+    axes[2].set_xlim([0, 80])
+    axes[2].set_ylim([40, 90])
+    axes[2].set_xlabel('X1')
+    axes[2].set_ylabel('X2')
+    axes[2].set_title('Classify')
+
+    epochs = np.arange(log.epochs)
+    line, = axes[0].plot([], [])
+
+    scatter1 = axes[1].scatter([], [], s=4, c='b')
+    scatter2 = axes[1].scatter([], [], s=4, c='r')
+    axes[1].legend(["training set", "test set"])
+
+    scatter3 = axes[2].scatter(X_train[:, 0], X_train[:, 1], c=y_train, marker='s')
+    scatter4 = axes[2].scatter(X_test[:, 0], X_test[:, 1], c=y_test, marker='^')
+    axes[2].legend(["training set", "test set"])
+    line0, = axes[2].plot([], [])
+    log.theta = np.asarray(log.theta)
+
+
+x = np.arange(0, 80, 0.8)
+
+
+def animate(i):
+    line.set_data(epochs[:i], log.loss[:i])  # update data
+
+    scatter1.set_offsets(np.stack((epochs[:i], ac_train[:i]), axis=1))  # update data
+    scatter2.set_offsets(np.stack((epochs[:i], ac_test[:i]), axis=1))
+
+    line0.set_data(x, -(log.theta[i][0] + log.theta[i][1] * (x - np.mean(X_train[:, 0])) / np.std(X_train[:, 0])) /
+                   log.theta[i][2] * np.std(X_train[:, 1]) + np.mean(X_train[:, 1]))
+
+
+ani = animation.FuncAnimation(fig, animate, interval=1, frames=log.epochs, repeat=False)
+# ani.save('Logistic.gif', writer='pillow', fps=100) #save the animation
+plt.show()
---
+++ 余恒Softmax.py
@@ -0,0 +1,144 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import warnings
+from matplotlib import animation
+
+warnings.simplefilter("error")
+
+X_train = np.loadtxt("D:/ML_DATA/iris/train/x.txt")
+y_train = np.loadtxt("D:/ML_DATA/iris/train/y.txt").astype('int64')
+
+X_test = np.loadtxt("D:/ML_DATA/iris//test/x.txt")
+y_test = np.loadtxt("D:/ML_DATA/iris/test/y.txt").astype('int64')
+
+
+
+
+class softmax:
+    def __init__(self, lr, epochs):
+        self.lr = lr  # learning rate
+        self.epochs = epochs  # circulation times
+
+    def softmax(self, z):
+        """
+        compute the sum of each row and reshape the matrix as one row
+        :param z:
+        :return:
+        """
+
+        return np.exp(z) / np.sum(np.exp(z), axis=1).reshape(-1, 1)
+
+    def fit(self, X, y):
+
+        self.X = np.insert(X, 0, 1, axis=1)
+        self.y = y
+        n_classes = len(np.unique(y))  # number of classes
+        np.random.seed(2)
+        self.w = [np.random.rand(self.X.shape[1], n_classes)]  # initialize weights
+
+        error = []  # list of the deviation of the real value and predicted value
+        self.loss = []  # list of loss
+
+        y = np.eye(len(y), np.sum(np.unique(y)))[y]  # one_hot encoding
+
+        for i in range(self.epochs):
+            y_ = self.softmax(np.dot(self.X, self.w[i]))
+
+            error.append(y - y_)  # add new error to the error list
+            J = -np.sum(np.multiply(np.log(y_), y))  # compute the loss
+            print("------------------")
+
+            self.loss.append(J)  # add new loss to the loss list
+            print("loss: ",J)
+            self.w.append(self.w[i] + self.lr * (np.dot(self.X.T, error[-1])))
+
+    # @jit
+    def predict(self, x, y):
+        """
+
+        :return: prediction
+        """
+
+        predict_, ac_ = self.y, []
+        for i in np.arange(self.epochs):
+            predict_ = (np.argmax(self.softmax(np.dot(x, self.w[i])), axis=1))
+            ac_.append(np.sum(predict_ == y) / len(y))
+
+        return ac_
+
+
+if __name__ == '__main__':
+
+    s = softmax(0.001, 500)
+
+    s.fit(X_train, y_train)
+    X_train_1 = np.insert(X_train, 0, 1, axis=1)
+    ac_train = s.predict(X_train_1, y_train)
+    X_test_1 = np.insert(X_test, 0, 1, axis=1)
+    ac_test = s.predict(X_test_1, y_test)
+
+    fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+
+    # Preparation for drawing
+    axes[0].set_xlim([0, 500])
+    axes[0].set_ylim([0, 150])
+    axes[0].set_xlabel('Iteration')
+    axes[0].set_ylabel('Loss')
+    axes[0].set_title('Loss')
+
+    axes[1].set_xlabel('X1')
+    axes[1].set_ylabel('X2')
+    axes[1].set_title('Classify')
+
+    axes[2].set_xlim([0, 500])
+    #
+    axes[2].set_ylim([0.2, 1])
+    axes[2].set_xlabel('Iteration')
+    axes[2].set_ylabel('Accuracy')
+    axes[2].set_title('Accuracy')
+
+    line0, = axes[0].plot([], [])
+
+    x1_min, x1_max = X_train[:, 0].min() - 1, X_train[:, 0].max() + 1
+    x2_min, x2_max = X_train[:, 1].min() - 0.1, X_train[:, 1].max() + 0.1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, 0.001),
+                           np.arange(x2_min, x2_max, 0.001))
+
+    X = np.array([xx1.ravel(), xx2.ravel()]).T
+    X = np.insert(X, 0, 1, axis=1)
+
+    axes[1].scatter(s.X[:, 1], s.X[:, 2], c=s.y, s=7, marker='s')
+    axes[1].scatter(X_test[:, 0], X_test[:, 1], c=y_test, s=7, marker='^')
+    plt.legend(["training set", "test set"])
+    epochs = np.arange(s.epochs)
+
+    Z = np.argmax(s.softmax(np.dot(X, s.w[0])), axis=1)
+
+    Z = Z.reshape(xx1.shape)
+    cont = axes[1].contourf(xx1, xx2, Z, alpha=0.2, cmap='brg')
+
+    scatter2 = axes[2].scatter([], [], s=3, c='b')
+    scatter3 = axes[2].scatter([], [], s=3, c='r')
+    plt.legend(["training set", "test set"])
+
+
+    def animate(i):
+
+        global cont
+        for c in cont.collections:
+            c.remove()
+        line0.set_data(epochs[:i], s.loss[:i])  # update data
+
+        scatter2.set_offsets(np.stack((epochs[:i], ac_train[:i]), axis=1))  # update data
+        scatter3.set_offsets(np.stack((epochs[:i], ac_test[:i]), axis=1))
+        Z = np.argmax(s.softmax(np.dot(X, s.w[i])), axis=1) #compute new hypothesis
+        Z = Z.reshape(xx1.shape)
+        cont = axes[1].contourf(xx1, xx2, Z, alpha=0.2, cmap='brg')
+
+
+
+    # plt.get_current_fig_manager().full_screen_toggle()
+
+    ani = animation.FuncAnimation(fig, animate, interval=1, frames=s.epochs, repeat=False)
+    #ani.save('softmax.gif', writer='pillow', fps=100) #save the animation
+    plt.show()
