214480b2e96322f41cd29604ef960d309b106ef6
govvc <64343137+govvc@users.noreply.github.com>
2020-11-03 09:32:43 +0800

    Update Softmax.py

--- Softmax.py
+++ Softmax.py
@@ -80,8 +80,8 @@ if __name__ == '__main__':
     fig, axes = plt.subplots(1, 3, figsize=(15, 5))
 
     # Preparation for drawing
-    axes[0].set_xlim([0, 500])
-    axes[0].set_ylim([0, 150])
+    axes[0].set_xlim([0, s.epochs])
+    axes[0].set_ylim([10, 150])
     axes[0].set_xlabel('Iteration')
     axes[0].set_ylabel('Loss')
     axes[0].set_title('Loss')
@@ -90,7 +90,7 @@ if __name__ == '__main__':
     axes[1].set_ylabel('X2')
     axes[1].set_title('Classify')
 
-    axes[2].set_xlim([0, 500])
+    axes[2].set_xlim([0, s.epochs])
     #
     axes[2].set_ylim([0.2, 1])
     axes[2].set_xlabel('Iteration')
@@ -101,25 +101,25 @@ if __name__ == '__main__':
 
     x1_min, x1_max = X_train[:, 0].min() - 1, X_train[:, 0].max() + 1
     x2_min, x2_max = X_train[:, 1].min() - 0.1, X_train[:, 1].max() + 0.1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, 0.001),
-                           np.arange(x2_min, x2_max, 0.001))
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, 0.01),
+                           np.arange(x2_min, x2_max, 0.01))
 
     X = np.array([xx1.ravel(), xx2.ravel()]).T
     X = np.insert(X, 0, 1, axis=1)
 
-    axes[1].scatter(s.X[:, 1], s.X[:, 2], c=s.y, s=7, marker='s')
-    axes[1].scatter(X_test[:, 0], X_test[:, 1], c=y_test, s=7, marker='^')
-    plt.legend(["training set", "test set"])
-    epochs = np.arange(s.epochs)
+    axes[1].scatter(s.X[:, 1], s.X[:, 2], c=s.y, s=10, marker='s')
+    axes[1].scatter(X_test[:, 0], X_test[:, 1], c=y_test, s=10, marker='^')
+    axes[1].legend(["training set", "test set"])
+    epochs = np.arange(0,s.epochs,10)
 
     Z = np.argmax(s.softmax(np.dot(X, s.w[0])), axis=1)
 
     Z = Z.reshape(xx1.shape)
     cont = axes[1].contourf(xx1, xx2, Z, alpha=0.2, cmap='brg')
 
-    scatter2 = axes[2].scatter([], [], s=3, c='b')
-    scatter3 = axes[2].scatter([], [], s=3, c='r')
-    plt.legend(["training set", "test set"])
+    scatter2 = axes[2].scatter([], [], s=5, c='b')
+    scatter3 = axes[2].scatter([], [], s=5, c='r')
+    axes[2].legend(["training set", "test set"])
 
 
     def animate(i):
@@ -127,10 +127,10 @@ if __name__ == '__main__':
         global cont
         for c in cont.collections:
             c.remove()
-        line0.set_data(epochs[:i], s.loss[:i])  # update data
+        line0.set_data(range(0,i), s.loss[:i])  # update data
 
-        scatter2.set_offsets(np.stack((epochs[:i], ac_train[:i]), axis=1))  # update data
-        scatter3.set_offsets(np.stack((epochs[:i], ac_test[:i]), axis=1))
+        scatter2.set_offsets(np.stack((range(0,i), ac_train[:i]), axis=1))  # update data
+        scatter3.set_offsets(np.stack((range(0,i), ac_test[:i]), axis=1))
         Z = np.argmax(s.softmax(np.dot(X, s.w[i])), axis=1) #compute new hypothesis
         Z = Z.reshape(xx1.shape)
         cont = axes[1].contourf(xx1, xx2, Z, alpha=0.2, cmap='brg')
@@ -139,6 +139,8 @@ if __name__ == '__main__':
 
     # plt.get_current_fig_manager().full_screen_toggle()
 
-    ani = animation.FuncAnimation(fig, animate, interval=1, frames=s.epochs, repeat=False)
+    ani = animation.FuncAnimation(fig, animate, interval=1, frames=epochs, repeat=False)
     #ani.save('softmax.gif', writer='pillow', fps=100) #save the animation
     plt.show()
+
+ 
